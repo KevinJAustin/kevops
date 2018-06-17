d83f4336c818356d982456d21b70c43cb066cf31
Kevin Austin <kevin@kja.org.uk>
2018-06-17 09:47:10 +0100

    Content update

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 rm -rf public/*
+hugo
 git add .
 git commit -m "Content update"
+git push
 aws s3 sync public/ s3://www.kevops.co.uk/ --delete --profile kevops
-echo "All done\n"
+echo "All done"
